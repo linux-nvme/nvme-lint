db241b18e7502474570c96cfd541daad8f7e7ad9
Karl Bonde Torp <k.torp@samsung.com>
2023-03-02 13:45:15 +0100

    version-bump: v0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="nvme-lint",
-    version="0.1.4",
+    version="0.1.5",
     author="Karl Bonde Torp",
     author_email="k.torp@samsung.com",
     description="Validate content of NVMe specification",
